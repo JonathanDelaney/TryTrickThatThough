3b92c9d92365f76dc3d48ebe168714ce8e5c62ab
JonathanDelaney <glibbidydelaney@gmail.com>
2021-04-23 17:46:26 +0000

    add stytle to set board options

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,16 @@
 $(window).bind('load', function() {
     $(".spinner-border").css("display", "none");
     $(".spinner-cont").css("height", "0px");
-    // $(".space-bg").css("display", "block");
 });
 
-$('.tic-button').click(function(){
-        console.log("JS working")
-    if ($('.player-turn').text() == "Guest's turn") {
-        console.log("guest")
-        //pass
-    } else {
-        $(this).css({'background-color': 'rgb(255, 103, 82)', 'filter': 'brightness(150%)'});
-        // $(this).prop('disabled', true);
-        console.log("player 1")
-    }
-});
+// $('.tic-button').click(function(){
+//         console.log("JS working")
+//     if ($('.player-turn').text() == "Guest's turn") {
+//         console.log("guest")
+//         //pass
+//     } else {
+//         $(this).css({'background-color': 'rgb(255, 103, 82)', 'filter': 'brightness(150%)'});
+//         // $(this).prop('disabled', true);
+//         console.log("player 1")
+//     }
+// });
