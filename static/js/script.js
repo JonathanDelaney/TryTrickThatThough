372dca23798d446eff9bd0c5ed2871ecb4621374
JonathanDelaney <glibbidydelaney@gmail.com>
2021-04-29 20:08:51 +0000

    change background to base and add buttress decoration to forms

--- static/js/script.js
+++ static/js/script.js
@@ -2,15 +2,3 @@ $(window).bind('load', function() {
     $(".spinner-border").css("display", "none");
     $(".spinner-cont").css("height", "0px");
 });
-
-// $('.tic-button').click(function(){
-//         console.log("JS working")
-//     if ($('.player-turn').text() == "Guest's turn") {
-//         console.log("guest")
-//         //pass
-//     } else {
-//         $(this).css({'background-color': 'rgb(255, 103, 82)', 'filter': 'brightness(150%)'});
-//         // $(this).prop('disabled', true);
-//         console.log("player 1")
-//     }
-// });
