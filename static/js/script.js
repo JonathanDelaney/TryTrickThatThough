c2277ce9e7d0fa6771699415cac8f50e99aeb612
JonathanDelaney <glibbidydelaney@gmail.com>
2021-05-05 20:56:47 +0000

    add tooltip with coordinate values on hover over game box

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,11 @@ $(window).bind('load', function() {
     $(".spinner-border").css("display", "none");
     $(".spinner-cont").css("height", "0px");
 });
+
+// $('form').hover(function() {
+//     let coords = $('input[type=hidden]', this).val();
+//     console.log(coords);
+//     $(this).append("<div id='tooltip-box'>" + coords + "</div>");
+//     }, function () {
+//     $("div#tooltip-box").remove();
+// })
