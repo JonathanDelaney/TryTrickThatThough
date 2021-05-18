5193f43572e7c98e49732a7dacc148ad7fb851d9
JonathanDelaney <glibbidydelaney@gmail.com>
2021-05-18 18:10:19 +0000

    delete commented out code

--- static/js/script.js
+++ static/js/script.js
@@ -2,11 +2,3 @@ $(window).bind('load', function() {
     $(".spinner-border").css("display", "none");
     $(".spinner-cont").css("height", "0px");
 });
-
-// $('form').hover(function() {
-//     let coords = $('input[type=hidden]', this).val();
-//     console.log(coords);
-//     $(this).append("<div id='tooltip-box'>" + coords + "</div>");
-//     }, function () {
-//     $("div#tooltip-box").remove();
-// })
